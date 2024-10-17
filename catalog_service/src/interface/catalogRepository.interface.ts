b483db9734a0279dfa4578ae8bdeb7adb1922a45
Ilya Savchenko <97258310+st1gmat@users.noreply.github.com>
2024-10-17 15:15:14 +0300

    delete ts orders & catalog

--- catalog_service/src/interface/catalogRepository.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ICatalogRepository {
-    create(data: any): Promise<{}>;
-    update(data: any): Promise<{}>;
-    delete(id: any): any;
-    find(data: any): Promise<[]>;
-    findOne(data: any): Promise<{}>;
-}
